3689a5933a40910e6b260ee4d22889e096eb6e3e
Stephen Samuels <stevesam1927@gmail.com>
2024-01-22 10:36:21 +0530

    added JMeter 02 module

--- Learning Phase/Week 1/Day 5/JMeter 5/Source Code/what-is-performance-testing/books/src/main/java/com/acme/books/repository/CategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.acme.books.repository;
-
-import com.acme.books.model.Category;
-import org.springframework.data.repository.CrudRepository;
-
-public interface CategoryRepository extends CrudRepository<Category, Long> {
-}
